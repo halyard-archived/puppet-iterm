85662063ecec375fbf17cce4afcd20f241c6f426
Les Aker <me@lesaker.org>
2015-06-12 09:12:41 -0400

    add defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,9 +8,24 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class iterm (
+  $prefs_path = "/Users/${::boxen_user}/Dropbox/iterm"
 ) {
   package { 'iterm2-nightly-halyard':
     provider    => 'brewcask',
     require     => Homebrew::Tap['halyard/casks']
+  } ->
+  osx_defaults { 'Enable iterm preference loading':
+    user    => $::boxen_user,
+    domain  => 'com.googlecode.iterm2',
+    key     => 'LoadPrefsFromCustomFolder',
+    value   => 1,
+    type    => 'integer'
+    require => Package['dropbox-halyard']
+  } ->
+  osx_defaults { 'Set iterm preference path':
+    user   => $::boxen_user,
+    domain => 'com.googlecode.iterm2',
+    key    => 'PrefsCustomFolder',
+    value  => $prefs_path
   }
 }
