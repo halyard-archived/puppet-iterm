090aa9c809f52fb179717fd22e5f04b087948ee6
Les Aker <me@lesaker.org>
2017-04-05 23:28:27 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,15 +13,15 @@ class iterm (
   package { 'iterm2-nightly-halyard':
     provider => 'brewcask',
     require  => Homebrew::Tap['halyard/casks']
-  } ->
-  boxen::osx_defaults { 'Enable iterm preference loading':
+  }
+  -> boxen::osx_defaults { 'Enable iterm preference loading':
     user   => $::boxen_user,
     domain => 'com.googlecode.iterm2',
     key    => 'LoadPrefsFromCustomFolder',
     value  => 1,
     type   => 'integer',
-  } ->
-  boxen::osx_defaults { 'Set iterm preference path':
+  }
+  -> boxen::osx_defaults { 'Set iterm preference path':
     user   => $::boxen_user,
     domain => 'com.googlecode.iterm2',
     key    => 'PrefsCustomFolder',
